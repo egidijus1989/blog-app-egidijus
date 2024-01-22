0fad8fc372d3af8939eb9c23007ed308194f61a9
egidijus <145454567+egidijus1989@users.noreply.github.com>
2024-01-22 22:47:21 +0200

    comment edit function

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,9 +1,17 @@
 import express from "express";
-import { createComment } from "../controllers/comment.controller.js";
+import {
+  createComment,
+  getPostComments,
+  likeComment,
+  editComment,
+} from "../controllers/comment.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, createComment);
+router.get("/getPostComments/:postId", getPostComments);
+router.put("/likeComment/:commentId", verifyToken, likeComment);
+router.put("/editComment/:commentId", verifyToken, editComment);
 
 export default router;
