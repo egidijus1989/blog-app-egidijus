a5675ad441f6f935970f67895a274223d64380d4
egidijus <145454567+egidijus1989@users.noreply.github.com>
2024-01-25 15:36:42 +0200

    finished job

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className="min-h-screen flex items-center">
+      <div className="max-w-2xl mx-autp p-3 text-center">
+        <div className="">
+          <h1 className="text-3xl font-semibold">About my blog</h1>
+        </div>
+      </div>
+    </div>
+  );
 }
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,5 +1,11 @@
 import React from "react";
+import CallToAction from "../components/CallToAction";
 
 export default function Projects() {
-  return <div>Projects</div>;
+  return (
+    <div className="max-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6">
+      <h1 className="text-6xl font-semibold p-3">Projects</h1>
+      <CallToAction />
+    </div>
+  );
 }
